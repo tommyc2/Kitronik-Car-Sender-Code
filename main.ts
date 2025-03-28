59bbd34961549830917490b259fbfddaa0d24143
Tommy Condon <114081733+tommyc2@users.noreply.github.com>
2025-03-28 15:35:16 +0000

    Release V1.0

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
-    radio.sendNumber(0) // Stops left motor
+    // Stops left motor
+    radio.sendNumber(0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    radio.sendString("forward_left") // Go forwards (left)
+    // Go forwards (left)
+    radio.sendString("forward_left")
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
-    radio.sendNumber(2) // Stops right motor
+    // Stops right motor
+    radio.sendNumber(2)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    radio.sendString("forward_right") // Go forwards (right)
+    // Go forwards (right)
+    radio.sendString("forward_right")
 })
 radio.setGroup(1)
 basic.showIcon(IconNames.Yes)
